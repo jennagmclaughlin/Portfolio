723fe13a75bf4caea6fce323abeebd7795b94722
jengmcla <89791791+jengmcla@users.noreply.github.com>
2023-11-21 15:11:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import react router dom
+import { Routes, Route, HashRouter } from "react-router-dom";
+// import pages
+import Home from "./Pages/Home";
+// import components
+import Footer from "./Components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Hashrouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Hashrouter>
+    );
+};
 
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import components
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
